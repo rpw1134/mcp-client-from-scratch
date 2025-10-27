cf3287d04b0d6bb6237cc6dc50bb574b820b5161
rpw1134 <ryanw1134@gmail.com>
2025-10-27 01:07:05 -0400

    some prompt tweaking. using 4o

--- src/mcp_client_scratch/routers/sessions.py
+++ src/mcp_client_scratch/routers/sessions.py
@@ -25,7 +25,7 @@ async def create_agent_session(request: dict, session_store: SessionStore = Depe
 
 @router.post("/{session_id}/agent/request")
 async def handle_agent_request(request: ChatRequest, session_id, vector_store: VectorStore = Depends(get_vector_store), openai_client: OpenAIClient = Depends(get_openai_client), session_store: SessionStore = Depends(get_session_store))-> dict:
-    # almost working, need to write agent responses back to session store, need to debug why I am storing the system prompt as a message, need to tweak the system prompt to not call a tool unless ALL necessary arguments are accessible, need to remove base tools from the clients so that they aren't considered in the embedding stuff.
+    # almost working, need to tweak the system prompt to not call a tool unless ALL necessary arguments are accessible (seems to be more model dependant), need to remove base tools from the clients so that they aren't considered in the embedding stuff, and need to find a way to add context of previous tools that are waiting on information so that the agent can populate the field afterwards.
     req = request.message
     new_message = ModelMessage(role="user", content=req)
     session_store.post_message(session_id, new_message)
@@ -34,6 +34,6 @@ async def handle_agent_request(request: ChatRequest, session_id, vector_store: V
     for tool in relevant_tools:
         del tool["hash"]
     relevant_tools = json.dumps(relevant_tools)
-    response_message = await openai_client.tool_selection_request(system_prompt=f"{SYSTEM_PROMPT_BASE} {BASE_TOOLS} {relevant_tools}", messages = [cast(ChatCompletionMessageParam, message) for message in session_messages])
+    response_message = await openai_client.tool_selection_request(system_prompt=f"{SYSTEM_PROMPT_BASE} {BASE_TOOLS} {relevant_tools}", messages = [cast(ChatCompletionMessageParam, message) for message in session_messages], model='gpt-4o')
     session_store.post_message(session_id, ModelMessage(role="assistant", content=response_message))
     return {"res":response_message, "relevant_tools_in_query": json.loads(relevant_tools), "all_session_messages": session_messages}
